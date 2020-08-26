f8ebef8cd9e656f72df6c04bee62e7f5a52e04e0
sushilkumar50 <bhardwajkumar50@gmail.com>
2020-08-27 00:15:52 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,33 @@ console.clear();
 
 interface Observer<T> {
   next(value: T): void;
-  error(err: any): void;
-  complete(): void;
+  error?(err: any): void;
+  complete?(): void;
 }
+
+function myObserver(observer: Observer<number>){
+
+  let i = 0;
+
+  const id = setInterval(() => {
+    if(i> 3) {
+      observer.complete();
+    }
+    observer.next(i++);
+    }, 1000);
+
+  return () => {
+    clearInterval(id)
+  }
+}
+
+let intrvl = myObserver({
+  next(value: number) {
+    console.log(value);
+  },
+  complete() {
+    console.log('done');
+  }
+})
+
+setTimeout(() => intrvl(), 5000);
